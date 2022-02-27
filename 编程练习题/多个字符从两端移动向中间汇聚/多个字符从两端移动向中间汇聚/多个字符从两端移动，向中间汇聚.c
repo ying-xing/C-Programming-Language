490831faf762a161ea4f859395f9b211a6fd102a
影形 <2339254049@qq.com>
2022-02-27 16:09:09 +0800

    多个字符从两端移动向中间汇聚

---
+++ 编程练习题/多个字符从两端移动向中间汇聚/多个字符从两端移动向中间汇聚/多个字符从两端移动，向中间汇聚.c
@@ -0,0 +1,27 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+#include <windows.h>//Sleep函数的头文件
+#include <string.h>
+
+int main(void)
+{
+	char arr1[] = {"Hello Word!!!!"};
+	char arr2[] = {"##############"};
+	int left = 0;
+	int right = strlen(arr1) - 1;
+
+	for (left = 0; left <= right; left++, right--)
+	{
+		printf("%s\n", arr2);
+
+		Sleep(1000);//Sleep函数单位是ms(毫秒)，该语句是休眠(停顿)1秒
+		system("cls");//该语句作用是清空屏幕
+
+		arr2[left] = arr1[left];
+		arr2[right] = arr1[right];
+	}
+	printf("%s\n", arr2);
+
+	return 0;
+}

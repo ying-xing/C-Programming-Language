d93963bae21fc2178e6b147793105f9967a81205
影形 <2339254049@qq.com>
2022-02-28 16:18:47 +0800

    打印1-100中3的倍数

---
+++ 编程练习题/打印1-100中3的倍数/打印1-100中3的倍数/打印1-100中3的倍数.c
@@ -0,0 +1,26 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+
+int main(void)
+{
+	int i = 0;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (0 == i % 3)
+		{
+			printf("%d ", i);
+
+		}
+	}
+	
+	printf("\n\n");
+
+	for (i = 3; i <= 100; i += 3)
+	{
+		printf("%d ", i);
+	}
+
+	return 0;
+}

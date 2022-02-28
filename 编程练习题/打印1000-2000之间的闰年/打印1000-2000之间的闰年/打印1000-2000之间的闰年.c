50fe5d3fe830f53629f530a5d646a3e65110057a
影形 <2339254049@qq.com>
2022-02-28 17:46:53 +0800

    打印1000-2000之间的闰年

---
+++ 编程练习题/打印1000-2000之间的闰年/打印1000-2000之间的闰年/打印1000-2000之间的闰年.c
@@ -0,0 +1,22 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+
+int main(void)
+{
+	int year = 0;
+	int count = 0;
+
+	for (year = 1000; year <= 2000; year++)
+	{
+		if (((0 == year % 4) && (0 != year % 100)) || (0 == year % 400))
+		{
+			printf("%d ", year);
+			count++;
+		}
+	}
+	printf("\n%d\n", count);
+
+
+	return 0;
+}

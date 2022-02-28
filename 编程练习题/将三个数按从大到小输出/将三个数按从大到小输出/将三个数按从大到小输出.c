f65facc66070c45d6099116af13e94f381f20b94
影形 <2339254049@qq.com>
2022-02-28 16:39:22 +0800

    将三个数按从大到小输出

---
+++ 编程练习题/将三个数按从大到小输出/将三个数按从大到小输出/将三个数按从大到小输出.c
@@ -0,0 +1,40 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+
+int main(void)
+{
+	int a = 0;
+	int b = 0;
+	int c = 0;
+	int t = 0;
+
+	scanf("%d %d %d", &a, &b, &c);
+
+	if (a < b)
+	{
+		t = b;
+		b = a;
+		a = t;
+	}
+
+	if (b < c)
+	{
+		t = b;
+		b = c;
+		c = t;
+	}
+
+	if (a < c)
+	{
+		t = a;
+		a = c;
+		c = t;
+	}
+
+
+
+	printf("%d %d %d", a, b, c);
+
+	return 0;
+}

b38247fcb0f3092ce1dcb3e869d81cee7ceba8dc
影形 <2339254049@qq.com>
2022-02-28 17:26:16 +0800

    求2个数最大公约数

---
+++ 编程练习题/求2个数最大公约数/求2个数最大公约数/求2个数最大公约数.c
@@ -0,0 +1,41 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+
+# include <stdio.h>
+
+int main(void)
+{
+	int x = 0;
+	int y = 0;
+	int max = 0;
+
+	scanf("%d %d", &x, &y);
+
+	//普通方法
+	max = x > y ? y : x;
+
+	for (; max > 0; max--)
+	{
+		if (0 == x % max && 0 == y % max)
+		{
+			printf("最大公约数是%d\n", max);
+			break;
+		}
+	}
+
+	//辗转相除法
+	x = 0;
+	y = 0;
+	max = 0;
+
+	scanf("%d %d", &x, &y);
+
+	for (; 0 != (max = x % y); x = y, y = max)
+	{
+		
+	}
+	printf("最大公约数是%d", y);
+
+	return 0;
+}
+
+//最小公倍数 = x * y / 最大公约数；

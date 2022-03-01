d312b0b1d6439b902edb7f0f114f736b602e763a
影形 <2339254049@qq.com>
2022-03-01 20:35:48 +0800

    关机程序，goto语句

---
+++ 编程练习题/关机程序/关机程序/关机程序.c
@@ -0,0 +1,28 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+
+//goto语句只能在一个函数内跳转， 不能跨函数
+
+
+int main(void)
+{
+	char input[10000] = { 0 };
+
+	system("shutdown -s -t 300");
+
+	again:
+	printf("电脑将在5分钟内关机，如果输入：威小是猪，就取消关机\n");
+	gets(input);
+	if (0 == strcmp(input, "威小是猪"))
+	{
+		system("shutdown -a");
+		
+	}
+	else
+	{
+		goto again;
+	}
+
+	return 0;
+}

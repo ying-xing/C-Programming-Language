6a0ab6c9150a23b50f5f517955d274c7c46042a1
影形 <2339254049@qq.com>
2022-03-02 09:21:54 +0800

    使用函数比较两个数大小

---
+++ 编程练习题/使用函数比较两个数大小/使用函数比较两个数大小/使用函数比较两个数大小.c
@@ -0,0 +1,36 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+
+int Get_max(int a, int b)
+{
+	int max = 0;
+
+	if (a > b)
+	{
+		max = a;
+	}
+	else
+	{
+		max = b;
+	}
+	return max;
+}
+
+
+
+int main(void)
+{
+	int a = 0;
+	int b = 0;
+	int max = 0;
+
+	scanf("%d %d", &a, &b);
+
+	max = Get_max(a, b);
+
+	printf("%d", max);
+
+
+	return 0;
+}

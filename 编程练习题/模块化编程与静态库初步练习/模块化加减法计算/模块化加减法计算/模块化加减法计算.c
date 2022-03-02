0eba831b61a73bf3be2d21bb1a407639cd7db75f
影形 <2339254049@qq.com>
2022-03-02 15:29:48 +0800

    模块化初步学习

---
+++ 编程练习题/模块化编程与静态库初步练习/模块化加减法计算/模块化加减法计算/模块化加减法计算.c
@@ -0,0 +1,27 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+#include "add.h"
+#include "sub.h"
+
+//导入静态库
+#pragma comment(lib, "静态库减法计算.lib")//导入静态库,第二个是静态库文件名
+
+int main(void)
+{
+	int a = 0;
+	int b = 0;
+	int c = 0;
+
+	a = 10;
+	b = 5;
+
+	c = Add(a, b);
+
+	printf("%d\n", c);
+
+	c = Sub(a, b);
+	printf("%d", c);
+
+	return 0;
+}
---
+++ 编程练习题/模块化编程与静态库初步练习/静态库减法计算/静态库减法计算/sub.h
@@ -0,0 +1,6 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+
+//实现减法的函数
+//x是被减数， y是减数
+//返回x-y的差
+int Sub(int x, int y);

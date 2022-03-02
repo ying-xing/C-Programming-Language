c9d267d03ee9ac5330690dd369e6091da884f2a8
影形 <2339254049@qq.com>
2022-03-02 09:55:44 +0800

    用函数交换两个变量的值

---
+++ 编程练习题/用函数交换两个变量的值/用函数交换两个变量的值/用函数交换两个变量的值.c
@@ -0,0 +1,29 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+
+void Swap(int *a, int *b)
+{
+	int z = 0;
+
+	z = *a;
+	*a = *b;
+	*b = z;
+}
+
+
+int main(void)
+{
+	int a = 0;
+	int b = 0;
+	
+	scanf("%d %d", &a, &b);
+
+	Swap(&a, &b);
+
+	printf("%d %d\n", a, b);
+
+
+
+	return 0;
+}

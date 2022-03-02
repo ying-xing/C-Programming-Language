53122ab95227c4383c2e6a4d7a8cd35578cf5340
影形 <2339254049@qq.com>
2022-03-02 14:26:21 +0800

    函数的链式访问printf

---
+++ 编程练习题/函数的链式访问printf/函数的链式访问printf/函数的链式访问printf.c
@@ -0,0 +1,15 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+
+# include <stdio.h>
+
+
+
+
+int main(void)
+{
+	printf("%d", printf("%d", printf("%d", 43))); //printf函数的功能是在屏幕上打印，返回值是打印在屏幕上的字符的个数
+
+
+
+	return 0;
+}

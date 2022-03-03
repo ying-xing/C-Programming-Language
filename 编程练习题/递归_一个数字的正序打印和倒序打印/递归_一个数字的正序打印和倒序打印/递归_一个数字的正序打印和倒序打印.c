9cb54b44432d39381a3d960612d6cd97a4031644
影形 <2339254049@qq.com>
2022-03-03 12:29:15 +0800

    用递归打印一个数字的正序和倒叙

---
+++ 编程练习题/递归_一个数字的正序打印和倒序打印/递归_一个数字的正序打印和倒序打印/递归_一个数字的正序打印和倒序打印.c
@@ -0,0 +1,35 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+
+void printf1(int num)
+{
+	if (num > 9)
+	{
+		printf1(num / 10);
+	}
+	printf("%d", num % 10);
+}
+
+void printf2(int num)
+{
+	printf("%d", num % 10);
+	if (num > 9)
+	{		
+		printf2(num / 10);
+	}
+	
+}
+
+int main(void)
+{
+	unsigned int num = 0;
+
+	scanf("%u", &num);
+
+	printf1(num);
+	printf("\n");
+	printf2(num);
+
+	return 0;
+}

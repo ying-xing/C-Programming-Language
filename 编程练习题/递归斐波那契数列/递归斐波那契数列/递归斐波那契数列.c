e2059e598e850f2bede29b8572ac752b1d813277
影形 <2339254049@qq.com>
2022-03-04 09:28:41 +0800

    递归求斐波那契数列

---
+++ 编程练习题/递归斐波那契数列/递归斐波那契数列/递归斐波那契数列.c
@@ -0,0 +1,26 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+
+int Fac(int n)
+{
+	if (n <= 2)
+	{
+		return 1;
+	}
+	else
+	{
+		return Fac(n - 1) + Fac(n - 2);
+	}
+}
+
+int main(void)
+{
+	int n = 0;
+	
+	scanf("%d", &n);
+	printf("%d\n", Fac(n));
+
+
+	return 0;
+}

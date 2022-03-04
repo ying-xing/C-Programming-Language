27b6ca70e10e28a6ddb65e654936d73d2805b63f
影形 <2339254049@qq.com>
2022-03-04 11:07:07 +0800

    循环实现求第n个斐波那契数

---
+++ 编程练习题/循环实现求第n个斐波那契数/循环实现求第n个斐波那契数/循环实现求第n个斐波那契数.c
@@ -0,0 +1,32 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+
+int Fib(n)
+{
+	int a = 1;
+	int b = 1;
+	int sum = 0;
+	int i = 0;
+
+	sum = 1;
+
+	for (i = 3;i<=n && n > 2; i++)
+	{
+		sum = a + b;
+		a = b;
+		b = sum;
+	}
+	return sum;
+}
+
+int main(void)
+{
+	int n = 0;
+
+	scanf("%d", &n);
+
+	printf("%d\n", Fib(n));
+
+	return 0;
+}

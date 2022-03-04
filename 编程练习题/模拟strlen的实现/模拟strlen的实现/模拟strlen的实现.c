5f0dade4eeba0ce1d863db1ae6501a6071c6ff8a
影形 <2339254049@qq.com>
2022-03-04 08:52:36 +0800

    模拟实现strlen,求字符串长度，不用临时变量，递归

---
+++ 编程练习题/模拟strlen的实现/模拟strlen的实现/模拟strlen的实现.c
@@ -0,0 +1,25 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+
+int My_strlen(char *parr)
+{
+	if ('\0' != *parr)
+	{
+		return 1 + My_strlen(parr + 1);
+	}
+	else
+	{
+		return 0;
+	}
+}
+
+int main(void)
+{
+	char arr[] = "haha\nhehe";
+
+	printf("%d\n", My_strlen(arr));
+
+
+	return 0;
+}

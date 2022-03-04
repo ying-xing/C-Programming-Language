7a45321a04e1905462c59b476e390ee13ed0398a
影形 <2339254049@qq.com>
2022-03-04 11:39:24 +0800

    求10个整数中最大值

---
+++ 编程练习题/求10个整数中最大值/求10个整数中最大值/求10个整数中最大值.c
@@ -0,0 +1,25 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+
+int main(void)
+{
+	int arr[10] = { 0 };
+	int max = 0;
+	int i = 0;
+	
+	scanf("%d", &arr[0]);
+	max = arr[0];
+
+	for (i = 1; i < 10; i++)
+	{
+		scanf("%d", &arr[i]);
+		if (max < arr[i])
+		{
+			max = arr[i];
+		}
+	}
+	printf("%d", max);
+
+	return 0;
+}

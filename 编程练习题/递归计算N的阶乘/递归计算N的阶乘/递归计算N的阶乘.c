ea032b580cf383085116473d44f0942ca7f89e53
影形 <2339254049@qq.com>
2022-03-04 09:03:24 +0800

    n的阶乘，递归实现

---
+++ 编程练习题/递归计算N的阶乘/递归计算N的阶乘/递归计算N的阶乘.c
@@ -0,0 +1,29 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+
+int Fac(int n)
+{
+	if (1 == n)
+	{
+		return 1;
+	}
+	else
+	{
+		return n * Fac(n - 1);
+	}
+}
+
+
+int main(void)
+{
+	int n = 0;
+	
+	scanf("%d", &n);
+
+	printf("%d\n", Fac(n));
+
+
+
+	return 0;
+}

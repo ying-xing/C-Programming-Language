2cebc71c52b838019f7c1496e46cc51ea4d8a37e
影形 <2339254049@qq.com>
2022-03-04 11:11:53 +0800

    1-100所有整数中出现多少个数字9

---
+++ 编程练习题/1-100所有整数中出现多少个数字9/1-100所有整数中出现多少个数字9/1-100所有整数中出现多少个数字9.c
@@ -0,0 +1,26 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+
+
+
+int main(void)
+{
+	int i = 0;
+	int count = 0;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (9 == i / 10)
+		{
+			count++;
+		}
+		if (9 == i % 10)
+		{
+			count++;
+		}
+	}
+	printf("%d", count);
+
+	return 0;
+}

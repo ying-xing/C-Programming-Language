e892b82be879795697d1334afe463d499c9016a6
影形 <2339254049@qq.com>
2022-03-05 11:21:11 +0800

    循环实现一个字符串内容逆序，不使用库函数

---
+++ 编程练习题/循环把字符串内容逆序/循环把字符串内容逆序/循环把字符串内容逆序.c
@@ -0,0 +1,48 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+#include <string.h>
+
+int My_strlen(char* arr)
+{
+	int count = 0;
+
+	while (( *arr) != '\0')
+	{
+		arr++;
+		count++;
+	}
+	return count;
+}
+
+void Reverse_string(char* arr)
+{
+	int left = 0;
+	int right = 0;
+	int len = 0;
+	char tmp = 0;
+
+	len = My_strlen(arr);
+	//printf("%d\n", len);
+	right = len - 1;
+	for (left = 0; left < right; left++, right--)
+	{
+		tmp = *(arr + left);
+		*(arr + left) = *(arr + right);
+		*(arr + right) = tmp;
+	}
+
+}
+
+int main(void)
+{
+	char arr[100] = { 0 };
+
+	gets(arr);
+
+	Reverse_string(arr);
+
+	puts(arr);
+
+	return 0;
+}

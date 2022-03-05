a974ab083c50e876350a0763a365f6d34f01120e
影形 <2339254049@qq.com>
2022-03-05 12:14:38 +0800

    递归实现把一个字符串内容逆序，改变原字符串，不能重新创建一个串，也不能使用库函数

---
+++ 编程练习题/递归-把一个字符串内容逆序/递归-把一个字符串内容逆序/递归-把一个字符串内容逆序.c
@@ -0,0 +1,47 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+#include <string.h>
+
+int My_strlen(char* str2)
+{
+	int count = 0;
+
+	for (; *str2 != '\0'; str2++)
+	{
+		count++;
+	}
+	return count;
+}
+
+void Reverse_string(char* str1)
+{
+	char tmp = 0;
+	int len = 0;
+
+	len = My_strlen(str1);
+	//printf("%d\n", len);
+	tmp = *str1;
+	*str1 = *(str1 + len - 1);
+	*(str1 + len - 1) = '\0';
+	if (My_strlen(str1 + 1) >= 2)
+		//	if (len >= 3)
+	{
+		Reverse_string(str1 + 1);
+	}
+	*(str1 + len - 1) = tmp;
+}
+
+int main(void)
+{
+	char arr[100] = { 0 };
+
+	gets(arr);
+
+	Reverse_string(arr);
+
+	puts(arr);
+
+
+	return 0;
+}

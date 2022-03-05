8460b784fcb2ac2f0bb56a050049f3b3cbb30f2e
影形 <2339254049@qq.com>
2022-03-05 12:31:06 +0800

    递归实现一个数每一位相加的和

---
+++ 编程练习题/递归_一个数每一位相加的和/递归_一个数每一位相加的和/递归_一个数每一位相加的和.c
@@ -0,0 +1,29 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+
+int DigitSum(int n)
+{
+	if (n > 9)
+	{
+		return DigitSum(n / 10) + n % 10;
+	}
+	else
+	{
+		return n;
+	}
+}
+
+int main(void)
+{
+	long long num = 0;
+	int sum = 0;
+	scanf("%d", &num);
+
+	sum = DigitSum(num);
+
+	printf("%d\n", sum);
+
+
+	return 0;
+}

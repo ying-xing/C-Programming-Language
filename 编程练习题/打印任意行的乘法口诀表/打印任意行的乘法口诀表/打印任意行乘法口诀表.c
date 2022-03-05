f4314f4ea6c799331b5b8e646c07ec6f2df76cd5
影形 <2339254049@qq.com>
2022-03-05 10:13:53 +0800

    打印任意行乘法口诀表

---
+++ 编程练习题/打印任意行的乘法口诀表/打印任意行的乘法口诀表/打印任意行乘法口诀表.c
@@ -0,0 +1,29 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+
+void Print_table(int n)
+{
+	int i = 0;
+	int j = 0;
+
+	for (i = 1; i <= n; ++i)
+	{
+		for (j = 1; j <= i; j++)
+		{
+			printf("%2d*%-2d=%-3d ", j, i, i*j);
+		}
+		printf("\n");
+	}
+}
+
+int main(void)
+{
+	int n = 0;
+
+	scanf("%d", &n);
+
+	Print_table(n);
+
+	return 0;
+}

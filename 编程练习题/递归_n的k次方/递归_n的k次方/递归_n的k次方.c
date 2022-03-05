b510f983374438e56b348a0bf55c029399bd685e
影形 <2339254049@qq.com>
2022-03-05 14:48:39 +0800

    递归实现n的k次方，pow函数

---
+++ 编程练习题/递归_n的k次方/递归_n的k次方/递归_n的k次方.c
@@ -0,0 +1,40 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+
+float Pow(int n, int k)
+{
+	if (k > 0)
+	{
+		return n * Pow(n, k-1);
+	}
+	else
+	{
+		if (0 == k)
+		{
+			return 1;
+		}
+		else
+		{
+			if (0 > k)
+			{
+				return 1.0/Pow(n, -k);
+			}
+		}
+	}
+}
+
+int main(void)
+{
+	int n = 0;
+	int k = 0;
+	float ret = 0;
+
+	scanf("%d %d", &n, &k);
+
+	ret = Pow(n, k);
+
+	printf("%f\n", ret);
+
+	return 0;
+}

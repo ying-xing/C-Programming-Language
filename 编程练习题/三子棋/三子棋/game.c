cb06b7c20f929af3faca6564eab9e84d6dfecd18
影形 <2339254049@qq.com>
2022-03-06 20:10:25 +0800

    N子棋

---
+++ 编程练习题/三子棋/三子棋/game.c
@@ -0,0 +1,246 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+//N子棋
+#include "game.h"
+
+//初始化棋盘
+void InitBoard(char board[ROW][COL], int row, int col)
+{
+	int i = 0;
+	int j = 0;
+	for (i = 0; i < row; i++)
+	{
+		for (j = 0; j < col; j++)
+		{
+			board[i][j] = ' ';
+		}
+	}
+}
+
+//打印棋盘
+void DisplayBoard(char board[][COL], int row, int col)
+{
+	int i = 0;
+
+	{
+		printf("|");
+		int j = 0;
+		for (j = 0; j < col; j++)
+		{
+			printf("---");
+			//if (j < col - 1)
+			{
+				printf("|");
+			}
+		}
+	}
+	printf("\n");
+
+	for (i = 0; i < row; i++)
+	{
+		int j = 0;
+		printf("|");
+		for (j = 0; j < col; j++)
+		{
+			printf(" %c ", board[i][j]);
+			//if (j < col - 1)
+			{
+				printf("|");
+			}
+		}
+		printf("\n");
+
+		printf("|");
+		//if (i < row - 1)
+		{
+			for (j = 0; j < col; j++)
+			{
+				printf("---");
+				//if (j < col - 1)
+				{
+					printf("|");
+				}
+			}
+		}
+		printf("\n");
+		
+
+	}
+}
+
+//玩家下棋
+void PlayerMove(char board[][COL], int row, int col)
+{
+	int x = 0;
+	int y = 0;
+	printf("轮到玩家下棋！\n");
+
+	while (1)
+	{
+		//printf("请输入下棋的位置：\n");
+		printf("\n----------------------------------------------------------\n");
+		printf("提示：\n");
+		printf("*代表玩家的棋子，#代表电脑的棋子\n");
+		printf("先输入行，然后输入列，中间用空格分隔，输完按回车继续\n");
+		printf("例如：输入2 3表示下在第二行第三列的位置\n");
+		printf("----------------------------------------------------------\n\n");
+		printf("请输入下棋的位置：\n");
+		scanf("%d %d", &x, &y);
+		if (x > 0 && x <= ROW && y > 0 && y <= COL)
+		{
+			if (' ' == board[x - 1][y - 1])
+			{
+				board[x - 1][y - 1] = '*';
+				break;
+			}
+			else
+			{
+				printf("\n该位置已有棋子，请重新下棋！\n");
+			}
+		}
+		else
+		{
+			printf("\n输入错误，请重新输入！\n");
+		}
+	}
+
+}
+
+
+//电脑下棋
+void ComputerMove(char board[][COL])
+{
+	int x = 0;
+	int y = 0;
+	printf("轮到电脑下棋！\n");
+	printf("正在加载......\n");
+	while (1)
+	{
+		x = rand() % ROW;
+		y = rand() % COL;
+		if (' ' == board[x][y])
+		{
+			board[x][y] = '#';
+			break;
+		}
+	
+	}
+	printf("加载完成\n");
+	printf("电脑已下完棋！\n\n");
+
+
+}
+
+//判断当前棋局状况
+	//玩家赢 *
+	//电脑赢 #
+	//平局   Q
+	//游戏继续 C
+char Iswin(char board[][COL])
+{
+	int x = 0;
+	int y = 0;
+	int i = 0;
+	int j = 0;
+	int l = 0;
+	//判断行列
+	for (i = 0; i < ROW; i++)
+	{
+		int count = 0;
+		for (j = 0; j < COL-1; j++)
+		{
+					
+			if (board[i][j] == board[i][j + 1] &&  board[i][j] != ' ')
+			{
+				count++;
+			}
+			else
+			{
+				break;
+			}
+			
+		}
+		if (COL - 1 == count)
+		{
+			return board[i][j];
+		}
+
+		count = 0;
+		for (l = 0; l < COL - 1; l++)
+		{
+
+			if (board[l][i] == board[l + 1][i] && board[l][i] != ' ')
+			{
+				count++;
+			}
+			else
+			{
+				break;
+			}
+
+		}
+		if (COL - 1 == count)
+		{
+			return board[l][i];
+		}
+	}
+
+	//判断对角线
+	int count = 0;
+	for (i = 0; i < ROW - 1; i++)
+	{
+
+		if (board[i][i] == board[i + 1][i + 1] && board[i][i] != ' ')
+		{
+			count++;		
+		}
+		if (COL - 1 == count)
+		{
+			return board[i][i];
+		}
+		//else
+		//{
+		//	break;
+		//}
+	}
+
+	count = 0;
+	for (i = 0, j = ROW - 1; i < ROW - 1; i++, j--)
+	{
+
+
+		if (board[i][j] == board[i+1][j - 1] && board[i][j] != ' ')
+		{
+			count++;
+		}
+		if (COL - 1 == count)
+		{
+			return board[j][j];
+		}
+		//else
+		//{
+		//	break;
+		//}	
+	}
+	int ret = Isfull(board);
+	return ret;
+}
+
+//判断棋盘是否下满
+//没满返回'C'
+//满了返回'Q'
+char Isfull(char board[][COL])
+{
+	int i = 0;
+	int j = 0;
+	for (i = 0; i < ROW; i++)
+	{
+		for (j = 0; j < COL; j++)
+		{
+			if (' ' == board[i][j])
+			{
+				return 'C';
+			}
+		}
+	}
+	return 'Q';
+}
---
+++ 编程练习题/三子棋/三子棋/test.c
@@ -0,0 +1,98 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+//N子棋
+#include "game.h"
+
+void menu(void)
+{
+	printf("******************************************************\n");
+	printf("********************* 1.开始游戏 *********************\n");
+	printf("********************* 2.退出游戏 *********************\n");
+	printf("**************该游戏最难玩法：让电脑获胜**************\n");
+	printf("******************************************************\n");
+}
+
+void game()
+{
+	//创造棋盘
+	char board[ROW][COL] = { 0 };
+
+	//初始化棋盘 初始化空格
+	InitBoard(board, ROW, COL);
+
+	//打印棋盘
+	DisplayBoard(board, ROW, COL);
+
+	char ret = 'C';//接收游戏状态
+	while (1)
+	{
+
+		//玩家下棋
+		PlayerMove(board, ROW, COL);
+		DisplayBoard(board, ROW, COL);
+		ret = Iswin(board);
+		if (ret != 'C')
+		{
+			break;
+		}
+
+		//电脑下棋
+		ComputerMove(board);
+		DisplayBoard(board, ROW, COL);
+		ret = Iswin(board);
+		if (ret != 'C')
+		{
+			break;
+		}
+	}
+	if ('*' == ret)
+	{
+		printf("玩家获胜！\n");
+		DisplayBoard(board, ROW, COL);
+		printf("恭喜你赢了！有没有兴趣挑战一下最难玩法？\n\n");
+	}
+	if ('#' == ret)
+	{
+		printf("电脑获胜！\n");
+		DisplayBoard(board, ROW, COL);
+		printf("你太棒了，在你的操作下让电脑获胜了！！！\n\n");
+	}
+	if ('Q' == ret)
+	{
+		printf("平局！\n");	
+		DisplayBoard(board, ROW, COL);
+		printf("你太棒了，在你的操作下让棋盘平局了！！\n有没有兴趣挑战一下最难玩法？\n\n");
+	}
+
+	printf("\n\n\n");
+}
+
+int main(void)
+{
+	int input = 0;
+
+	srand((unsigned int)time(NULL));
+
+	do
+	{
+		menu();
+		printf("\n请根据上述提示输入,输完按回车键！\n\n");
+		scanf("%d", &input);
+		switch (input)
+		{
+		case 1:
+			printf("\n游戏开始!\n");
+			game();
+			break;
+		case 2:
+			printf("\n游戏已退出！\n");
+			break;
+		default:
+			printf("\n输入错误，请重新输入!\n");
+		}
+		printf("\n\n");
+
+	} while (2 != input);
+
+
+	return 0;
+}

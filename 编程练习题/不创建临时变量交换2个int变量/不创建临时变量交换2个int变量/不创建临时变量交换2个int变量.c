a72329d46f632b196a311814d2055cf645237a9b
影形 <2339254049@qq.com>
2022-03-07 17:30:05 +0800

    不创建临时变量交换2个int变量

---
+++ 编程练习题/不创建临时变量交换2个int变量/不创建临时变量交换2个int变量/不创建临时变量交换2个int变量.c
@@ -0,0 +1,26 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+
+
+int main(void)
+{
+	int a = 5;
+	int b = 3;
+
+	//方法一：有缺陷，数字太大不行，会溢出
+	printf("a=%d b=%d\n", a, b);
+	a = a + b;
+	b = a - b;
+	a = a - b;
+	printf("a=%d b=%d\n", a, b);
+
+	//方法二:不会溢出
+	a = a ^ b;
+	b = a ^ b;
+	a = a ^ b;
+	printf("a=%d b=%d\n", a, b);
+
+
+	return 0;
+}

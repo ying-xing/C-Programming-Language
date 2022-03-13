611b0b9cf47136a1b3be86ad76ccd1f2954d8d0b
影形 <2339254049@qq.com>
2022-03-13 18:02:57 +0800

    指针相减模拟strlen

---
+++ 编程练习题/指针相减模拟strlen/指针相减模拟strlen/指针相减模拟strlen.c
@@ -0,0 +1,28 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1		//解决scanf函数在VS中报错
+
+#include <stdio.h>
+
+int Mystrlen(char *str)
+{
+	char* start = str;
+
+	for (; *str != '\0'; str++)
+	{
+
+	}
+
+	return str - start;
+
+}
+
+int main(void)
+{
+	char str[] = "asddf1we2";
+	int len = 0;
+
+	len = Mystrlen(str);
+
+	printf("%d\n", len);
+
+	return 0;
+}

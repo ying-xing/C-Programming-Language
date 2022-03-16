f5b9ca79be852e2e5049f6278b4cb64baa26066c
影形 <2339254049@qq.com>
2022-03-16 12:29:24 +0800

    strcpy函数

---
+++ 编程练习题/strcpy函数/strcpy函数/strcpy函数.c
@@ -0,0 +1,24 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1		//解决scanf函数在VS中报错
+
+#include <stdio.h>
+#include <assert.h>
+
+char* My_strcpy(const char* const destintion, const char* source)
+{
+	for (char* ret = destintion; *ret++ = *source++; )
+	{
+		;
+	}
+	return  destintion;
+}
+
+int main(void)
+{
+	char str1[40] = "xxxxxxxxxxxxxxxxxxxxxxxxxxx";
+	char str2[] = "hello\0你好";	
+
+	printf("%s", My_strcpy(str1, str2));
+
+
+	return 0;
+}

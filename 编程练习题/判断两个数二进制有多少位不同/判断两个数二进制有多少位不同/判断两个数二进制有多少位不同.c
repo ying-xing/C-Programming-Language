74a29130e5c68cc279a904f20b4a1916a1010a5a
影形 <2339254049@qq.com>
2022-03-16 18:34:53 +0800

    判断两个整数的二进制位中不同的位数

---
+++ 编程练习题/判断两个数二进制有多少位不同/判断两个数二进制有多少位不同/判断两个数二进制有多少位不同.c
@@ -0,0 +1,31 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1		//解决scanf函数在VS中报错
+
+#include <stdio.h>
+
+int NumberOf1(int ret)
+{
+	int count = 0;
+	for ( ; 0 != ret; count++)
+	{
+		ret = ret & (ret - 1);
+	}
+	return count;
+}
+
+int main(void)
+{
+	int a = 0;
+	int b = 0;
+	int count = 0;
+
+	scanf("%d %d", &a, &b);
+
+	int ret = a ^ b;
+	count = NumberOf1(ret);
+
+	printf("%d\n", count);
+
+	system("pause");
+
+	return 0;
+}

3a72f052e107e1ce1593e6dde8719cade6496ba2
影形 <2339254049@qq.com>
2022-03-16 17:10:15 +0800

    求一个整数的二进制位的1的个数

---
+++ 编程练习题/求一个整数二进制位1的个数/求一个整数二进制位1的个数/求一个整数二进制位1的个数.c
@@ -0,0 +1,26 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1		//解决scanf函数在VS中报错
+
+#include <stdio.h>
+
+int NumberOf1(int n)
+{
+	int count = 0;
+	while ( n != 0 )
+	{
+		n = n & (n - 1);
+		count++;
+	}
+	return count;
+}
+
+int main(void)
+{
+	int n = -1;
+	int ret = NumberOf1(n);
+
+	printf("%d\n", ret);
+
+	system("pause");
+
+	return 0;
+}

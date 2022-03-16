947e2f218868f60f3b6e7285b47b61b1139c7d45
影形 <2339254049@qq.com>
2022-03-16 18:04:04 +0800

    判断一个数是不是2的幂

---
+++ 编程练习题/判断一个数是不是2的n次方/判断一个数是不是2的n次方/判断一个数是不是2的n次方.c
@@ -0,0 +1,22 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1		//解决scanf函数在VS中报错
+
+#include <stdio.h>
+
+int PowerOf(int n)
+{
+	return ((n & (n - 1)) == 0);
+}
+
+int main(void)
+{
+
+	for (int i=0; i<=300; i++)
+		if ( 1 == PowerOf(i) )
+		{
+			printf("%d\n", i);
+		}
+
+	system("pause");
+
+	return 0;
+}

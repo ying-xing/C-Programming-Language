3f4ae4e9636f67b9300919835c7bdb59965625d3
影形 <2339254049@qq.com>
2022-03-16 21:15:21 +0800

    打印一个数二进制位奇数位和偶数位

---
+++ 编程练习题/打印一个数二进制位奇数位和偶数位/打印一个数二进制位奇数位和偶数位/打印一个数二进制位奇数位和偶数位.c
@@ -0,0 +1,27 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1		//解决scanf函数在VS中报错
+
+#include <stdio.h>
+
+
+
+int main(void)
+{
+	int a = 0;
+
+	scanf("%d", &a);
+
+	for (int i = 31; i >= 1; i -= 2)
+	{
+		printf("%d", (a >> i) & 1);
+	}
+	printf("\n");
+	for (int i = 30; i >= 0; i -= 2)
+	{
+		printf("%d", (a >> i) & 1);
+	}
+	printf("\n");
+
+	system("pause");
+
+	return 0;
+}

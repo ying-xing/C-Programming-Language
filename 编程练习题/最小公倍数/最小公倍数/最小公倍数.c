f4eee775748108efe2fbdf6f2d85a5474b89c163
影形 <2339254049@qq.com>
2022-03-18 16:21:54 +0800

    最小公倍数

---
+++ 编程练习题/最小公倍数/最小公倍数/最小公倍数.c
@@ -0,0 +1,25 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1		//解决scanf函数在VS中报错
+
+#include <stdio.h>
+
+int main(void)
+{
+	int a = 0;
+	int b = 0;
+
+	scanf("%d %d", &a, &b);
+
+	for (int i = 1; ; i++)
+	{
+		if (0 == a * i % b)
+		{
+			printf("%d\n", a * i);
+			break;
+		}
+	}
+	
+
+	system("pause");
+
+	return 0;
+}

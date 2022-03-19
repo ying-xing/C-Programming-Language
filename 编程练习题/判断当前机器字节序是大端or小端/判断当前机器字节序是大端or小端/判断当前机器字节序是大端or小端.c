3446e11efd7fb3f8a6bad6fa817c7d63b0d1f8bf
影形 <2339254049@qq.com>
2022-03-19 12:34:02 +0800

    判断当前机器字节序是大端还是小端

---
+++ 编程练习题/判断当前机器字节序是大端or小端/判断当前机器字节序是大端or小端/判断当前机器字节序是大端or小端.c
@@ -0,0 +1,29 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1		//解决scanf函数在VS中报错
+
+#include <stdio.h>
+
+int CheckSys(void)
+{
+	int i = 1;
+	char* p = (char*)&i;
+	return *p;//返回1是小端，0是大端
+}
+
+int main(void)
+{
+	int ret = CheckSys();
+
+	if (1 == ret)
+	{
+		printf("小端\n");
+	}
+	else
+	{
+		printf("大端\n");
+	}
+
+
+	system("pause");
+
+	return 0;
+}

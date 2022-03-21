d64ed0962126473817f32adebec3de0da12e02dd
影形 <2339254049@qq.com>
2022-03-21 17:10:31 +0800

    测试scanf("%d\n", &a);

---
+++ 编程练习/scanf函数测试/scanf函数测试/scanf测试.c
@@ -0,0 +1,16 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1		//解决scanf函数在VS中报错
+
+#include <stdio.h>
+
+int main(void)
+{
+	int a = 0;
+
+	scanf("%d\n", &a);	//需要输入一个数和/
+
+	printf("%d\n", a);
+
+	system("pause");
+
+	return 0;
+}

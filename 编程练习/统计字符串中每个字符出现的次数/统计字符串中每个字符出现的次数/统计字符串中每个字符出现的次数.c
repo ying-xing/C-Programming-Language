abafcaa40203fbb7e228b0d7ce1a2537b8829016
影形 <2339254049@qq.com>
2022-03-25 22:49:13 +0800

    统计字符串中每个字符出现的次数

---
+++ 编程练习/统计字符串中每个字符出现的次数/统计字符串中每个字符出现的次数/统计字符串中每个字符出现的次数.c
@@ -0,0 +1,31 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1		//解决scanf函数在VS中报错
+
+#include <stdio.h>
+
+int main(void)
+{
+	char str[20] = { 0 };
+	int count[26] = { 0 };
+
+	gets(str);
+
+	int len = strlen(str);
+	printf("%d\n", len);
+
+	for (int i = 0; i < len; i++)
+	{
+		count[str[i] - 'a']++;
+	}
+
+	for (int i = 0; i < 26; i++)
+	{
+		if (0 != count[i])
+		{
+			printf("%c字符有%d个\n", i + 'a', count[i]);
+		}
+	}
+
+	system("pause");
+
+	return 0;
+}

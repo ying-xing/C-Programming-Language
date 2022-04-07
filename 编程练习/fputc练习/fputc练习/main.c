c2ee39f5ce55ec8c32fde76cdc1a4eb08fbca948
影形 <2339254049@qq.com>
2022-04-07 15:05:35 +0800

    fputc练习

---
+++ 编程练习/fputc练习/fputc练习/main.c
@@ -0,0 +1,31 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1		//解决scanf函数在VS中报错
+
+#include <stdio.h>
+
+int main(void)
+{
+	char* filename = "test.txt";
+
+	FILE* fp = fopen(filename, "w");
+	if (NULL == fp)
+	{
+		perror("fopen error");
+		return -1;
+	}
+	
+	for (char ch = 'A'; ch <= 'Z'; ch++)
+	{
+		int ret = fputc(ch, fp);
+		if (-1 == ret)
+		{
+			perror("fputs error");
+			return -1;
+		}
+	}
+	fclose(fp);
+	printf("------------------------\n");
+
+	system("pause");
+
+	return 0;
+}
